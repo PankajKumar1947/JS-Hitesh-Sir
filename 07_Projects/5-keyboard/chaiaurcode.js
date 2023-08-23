146768398b7d593de110a5204f8b4664e40e8c65
Pankaj <pankaj.ky3007@gmail.com>
2023-08-23 20:26:32 +0530

    Adding ascci in table

--- 07_Projects/5-keyboard/chaiaurcode.js
+++ 07_Projects/5-keyboard/chaiaurcode.js
@@ -3,20 +3,23 @@ console.log("Project-5");
 const insert=document.getElementById('insert');
 
 window.addEventListener('keydown',(e)=>{
+  //on pressing any key everything on window screen should be replaced by the below elements.
     insert.innerHTML=`
      <div class='color'>
-     <table>
-     <tr>
-       <th>Key</th>
-       <th>Keycode</th>
-       <th>Code</th>
-     </tr>
-     <tr>
-       <td>${e.key===" "? "Space" :e.key}</td>
-       <td>${e.keyCode}</td>
-       <td>${e.code}</td>
-     </tr>
-   </table>
+       <table>
+         <tr>
+           <th>Key</th>
+           <th>Keycode</th>
+           <th>Code</th>
+           <th>ASCII</th>
+         </tr>
+         <tr>
+           <td>${e.key===" "? "Space" :e.key}</td>
+           <td>${e.keyCode}</td>
+           <td>${e.code}</td>
+           <td>${e.key.codePointAt(0)}</td>
+         </tr>
+        </table>
      </div>
     `
 })
