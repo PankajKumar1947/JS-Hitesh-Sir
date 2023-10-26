8b496b243b1cee0041030e0ee3c1e8bf432906d1
Pankaj <pankaj.ky3007@gmail.com>
2023-10-26 11:21:21 +0530

    while and dowhile loop

---
+++ 05_controlflowAnditerations/whileAndDowhile.js
@@ -0,0 +1,27 @@
+//while loop
+let i=0;
+while(i<=10){
+    console.log(`Value of index is ${i}`);
+    i=i+2;
+}
+
+let myAray=['flash','batman','superman'];
+let arr=0;
+while(arr<myAray.length){
+    console.log(`Value if ${myAray[arr]}`);
+    arr=arr+1;
+}
+
+// do while loop
+//syntax
+// do {
+    
+// } while (condition);
+
+let score=100;
+do{
+    console.log(`Score is ${score}`);
+    score++;
+} while(score<=10)
+
+//do while loop runs the atleast one times whatever the condition.
